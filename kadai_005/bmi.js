4e144543ddd0ebf4d1188a00026e874dc291a046
Nb-Coding <128985854+Nb-Coding@users.noreply.github.com>
2024-11-05 17:07:24 +0900

    軽微な修正

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -2,8 +2,8 @@
 //身長：1.7m
 //計算式：[体重(kg)]÷[身長(m)×身長(m)]
 
-const userWeight = 68 ;
-const userHeight = 1.7;
+let userWeight = 68;
+let userHeight = 1.7;
 
 let userBmi = userWeight / (userHeight ** 2);
 
